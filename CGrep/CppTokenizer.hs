f7dacfe5516e715e4121c12ace0c55fdd4588614
Nicola Bonelli <nicola.bonelli@gmail.com>
2013-04-14 19:47:23 +0200

    CppTokenizer: new implementation

--- CGrep/CppTokenizer.hs
+++ CGrep/CppTokenizer.hs
@@ -1,7 +1,5 @@
 module CGrep.CppTokenizer (cgrepCppTokenizer) where
 
-import Data.ByteString.Lazy.Search as LC
-
 import qualified Data.ByteString.Char8 as C
 import qualified Data.ByteString.Lazy.Char8 as LC
 
@@ -12,16 +10,16 @@ import CGrep.Options
 import qualified CGrep.Cpp.Filter as Cpp
 import qualified CGrep.Cpp.Token  as Cpp
 
+
 cgrepCppTokenizer :: CgrepFunction
 cgrepCppTokenizer opt ps f = do
-    source <- LC.readFile f
-    let filtered =  Cpp.filter (mkContextFilter opt) source
-    let tks = Cpp.tokens filtered
-    let content = zip [1..] $ LC.lines filtered
-    print $ filter (Cpp.tokenFilter (tokens opt)) tks
-    return $ concat $ map (if (word opt) then simpleWordGrep opt f lps
-                                         else simpleLineGrep opt f ps) content
-        where lps = map LC.fromChunks (map (:[]) ps)
+    src <- LC.readFile f
+    let source   = Cpp.filter (mkContextFilter opt) src
+    let tks      = filter (Cpp.tokenFilter $ tokens opt) (Cpp.tokens source)
+    let content  = LC.lines source
+    let tks_res  = simpleTokenGrep opt f lps tks 
+    return $ map (\t -> let ln = fromIntegral (Cpp.lineno t) in LazyOutput f ln (content !! ln) [] ) tks_res
+        where lps = map C.unpack ps
 
 
 mkContextFilter :: Options -> Cpp.ContextFilter
@@ -30,20 +28,9 @@ mkContextFilter opt = if (not (code opt && comment opt && string opt) )
                        else Cpp.ContextFilter { Cpp.getCode = code opt, Cpp.getComment = comment opt, Cpp.getLiteral = string opt }
 
 
-simpleLineGrep :: Options -> FilePath -> [C.ByteString] -> (Int, LC.ByteString) -> [Output]
-simpleLineGrep opt f ps (n, l) = 
-   if ((null tks) `xor` (invert_match opt)) 
-     then []
-     else [LazyOutput f n l (map C.unpack tks)]
-   where tks  = filter (\p -> not . null $ LC.indices p l) ps   
+simpleTokenGrep :: Options -> FilePath -> [String] -> [Cpp.Token] -> [Cpp.Token]
+simpleTokenGrep opt _ ps ts = filter (\t -> (Cpp.toString t `elem` ps) `xor` invert_match opt) ts
 
 
-
-simpleWordGrep :: Options -> FilePath -> [LC.ByteString] -> (Int, LC.ByteString) -> [Output]
-simpleWordGrep opt f ps (n, l) = 
-   if ((null tks) `xor` (invert_match opt)) 
-     then []
-     else [LazyOutput f n l (map LC.unpack tks)]
-   where tks  = filter (`elem` (LC.words l)) ps   
 
 
