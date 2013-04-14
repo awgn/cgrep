34f749381030823f64068997b3ba9ad46833ec8e
Nicola Bonelli <nicola.bonelli@cnit.it>
2013-04-14 11:53:43 +0200

    Switched back to unpack version

--- CGrep/Output.hs
+++ CGrep/Output.hs
@@ -8,7 +8,7 @@ data Output = Output FilePath Int C.ByteString [C.ByteString]
 
 
 showOutput :: Options -> Output -> String
-showOutput Options { no_filename = False, no_linenumber = False } (Output f n l _) = f ++ ":" ++ show n ++ ":" ++ show l
-showOutput Options { no_filename = False, no_linenumber = True  } (Output f _ l _) = f ++ ":" ++ show l
-showOutput Options { no_filename = True , no_linenumber = False } (Output _ n l _) = show n ++ ":" ++ show l
-showOutput Options { no_filename = True , no_linenumber = True  } (Output _ _ l _) = show l
+showOutput Options { no_filename = False, no_linenumber = False } (Output f n l _) = f ++ ":" ++ show n ++ ":" ++ C.unpack l
+showOutput Options { no_filename = False, no_linenumber = True  } (Output f _ l _) = f ++ ":" ++ C.unpack l
+showOutput Options { no_filename = True , no_linenumber = False } (Output _ n l _) = show n ++ ":" ++ C.unpack l
+showOutput Options { no_filename = True , no_linenumber = True  } (Output _ _ l _) = C.unpack l
