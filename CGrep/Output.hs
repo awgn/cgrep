be617c3f8275b9d7efa79c09dd5205cd852a30fb
Nicola Bonelli <nicola.bonelli@gmail.com>
2013-04-18 11:03:04 +0200

    Output: switched to existential quantification..

--- CGrep/Output.hs
+++ CGrep/Output.hs
@@ -1,23 +1,19 @@
+{-# LANGUAGE ExistentialQuantification #-} 
+
 module CGrep.Output where
 
 import qualified Data.ByteString.Char8 as C
 import qualified Data.ByteString.Lazy.Char8 as LC
 
 import CGrep.Options
+import CGrep.StringLike
 
-data Output = StrictOutput FilePath Int  C.ByteString [String] |
-              LazyOutput   FilePath Int LC.ByteString [String] 
-                deriving (Show,Eq)
+data Output = forall a. (StringLike a) => Output FilePath Int a [String]
 
 
 showOutput :: Options -> Output -> String
-showOutput Options { no_filename = False, no_linenumber = False } (StrictOutput f n l _) = f ++ ":" ++ show n ++ ":" ++ C.unpack l
-showOutput Options { no_filename = False, no_linenumber = True  } (StrictOutput f _ l _) = f ++ ":" ++ C.unpack l
-showOutput Options { no_filename = True , no_linenumber = False } (StrictOutput _ n l _) = show n ++ ":" ++ C.unpack l
-showOutput Options { no_filename = True , no_linenumber = True  } (StrictOutput _ _ l _) = C.unpack l
+showOutput Options { no_filename = False, no_linenumber = False } (Output f n l _) = f ++ ":" ++ show n ++ ":" ++ toString l
+showOutput Options { no_filename = False, no_linenumber = True  } (Output f _ l _) = f ++ ":" ++ toString l
+showOutput Options { no_filename = True , no_linenumber = False } (Output _ n l _) = show n ++ ":" ++ toString l
+showOutput Options { no_filename = True , no_linenumber = True  } (Output _ _ l _) = toString l
 
-
-showOutput Options { no_filename = False, no_linenumber = False } (LazyOutput f n l _) = f ++ ":" ++ show n ++ ":" ++ LC.unpack l
-showOutput Options { no_filename = False, no_linenumber = True  } (LazyOutput f _ l _) = f ++ ":" ++ LC.unpack l
-showOutput Options { no_filename = True , no_linenumber = False } (LazyOutput _ n l _) = show n ++ ":" ++ LC.unpack l
-showOutput Options { no_filename = True , no_linenumber = True  } (LazyOutput _ _ l _) = LC.unpack l
