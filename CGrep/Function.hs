6d70041e5683263caa79cf2a8285c07ceb22c98e
Nicola Bonelli <nicola.bonelli@gmail.com>
2013-04-18 11:02:17 +0200

    Added basicGrep generic function...

--- CGrep/Function.hs
+++ CGrep/Function.hs
@@ -2,6 +2,7 @@ module CGrep.Function where
 
 import CGrep.Options
 import CGrep.Output
+import CGrep.StringLike
 
 import qualified Data.ByteString.Char8 as C
 import qualified Data.ByteString.Lazy.Char8 as LC
@@ -21,3 +22,12 @@ strictReadFile f  = C.readFile f
 lazyReadFile :: FilePath -> IO LC.ByteString
 lazyReadFile "" = LC.getContents
 lazyReadFile f  = LC.readFile f
+
+
+basicGrep :: (StringLike a) => Options -> FilePath -> [a] -> (Int, a) -> [Output]
+basicGrep opt f patterns (n, line) =
+    if null patfilt
+      then []
+      else [Output f n line (map toString patfilt)]
+    where patfilt = grep (word opt) (ignore_case opt) (invert_match opt) patterns line  
+
