25704d0a253fd3e18f9fa429048fa17b38c2111b
Nicola Bonelli <nicola.bonelli@gmail.com>
2013-04-25 16:55:38 +0200

    Context Filter with Language support

--- CGrep/ContextFilter.hs
+++ CGrep/ContextFilter.hs
@@ -19,8 +19,11 @@
 
 module CGrep.ContextFilter (Context(..), ContextFilter(..), filterContext)  where
 
-import qualified Data.ByteString.Char8 as C
+import CGrep.Lang
 
+import qualified Data.ByteString.Char8 as C
+import qualified Data.Map as Map
+import Data.Maybe
 
 type Source = C.ByteString
 
@@ -35,39 +38,117 @@ data ContextFilter = ContextFilter { getCode    :: Bool,
                      } deriving (Eq, Show)
 
 
-filterContext :: ContextFilter -> Source -> Source
-filterContext filt src =  snd $ C.mapAccumL runFilter (FiltState CodeState filt ' ') src 
+
+filterContext :: Maybe Lang -> ContextFilter -> Source -> Source
+
+filterContext Nothing     _ src =  src 
+filterContext (Just lang) filt src =  snd $ C.mapAccumL (fromJust $ Map.lookup lang filterMap) (FiltState StateCode filt []) src 
+
+
+-- runFilter map:
+--
+    
+type FilterType =  FiltState -> Char -> (FiltState, Char) 
+
+filterMap :: Map.Map Lang FilterType
+
+filterMap = Map.fromList [
+            (Awk,        filterUndefined),
+            (C,          runFilterCpp),
+            (Cpp,        runFilterCpp),
+            (Csharp,     runFilterCpp),
+            (Css,        filterUndefined),
+            (CMake,      filterUndefined),
+            (D,          runFilterCpp),
+            (Erlang,     filterUndefined),
+            (Fsharp,     filterUndefined),
+            (Go,         runFilterCpp),
+            (Haskell,    filterUndefined),
+            (Html,       filterUndefined),
+            (Java,       runFilterCpp),
+            (Javascript, runFilterCpp),
+            (Latex ,     filterUndefined),
+            (LUA ,       filterUndefined),
+            (Make,       filterUndefined),
+            (OCaml ,     filterUndefined),
+            (ObjectiveC, runFilterCpp),
+            (Perl,       filterUndefined),
+            (PHP,        filterUndefined),
+            (Python,     filterUndefined),
+            (Ruby,       filterUndefined),
+            (Scala,      runFilterCpp),
+            (Tcl,        filterUndefined),
+            (Shell,      filterUndefined),
+            (Verilog,    runFilterCpp),
+            (Vim,        filterUndefined)
+           ]
 
 
 -- States:
 
-data FiltState = FiltState {
+data FiltState = FiltState 
+                 {
                     cstate  :: ContextState,
                     cfilter :: ContextFilter,
-                    pchar   :: Char
-                } deriving (Eq, Show)
+                    pchar   :: [Char]
+                 } deriving (Eq, Show)
 
 
 
-data ContextState = CodeState       | 
-                    CommentCState   | 
-                    CommentCppState | 
-                    LiteralStateS   |
-                    LiteralStateC
+data ContextState = StateCode       | 
+                    StateComment    | 
+                    StateComment2   | 
+                    StateLiteral    |
+                    StateLiteral2
                         deriving (Eq, Show)
 
+-- filter functions:
+--
 
-runFilter :: FiltState -> Char -> (FiltState, Char) 
-runFilter filtstate c = (state', charFilter (cxtFilter cxt (cfilter filtstate)) c)
-                        where (cxt, state') = charParser (pchar filtstate, c) filtstate
+filterUndefined :: FiltState -> Char -> (FiltState, Char) 
+filterUndefined s c = (s, c)
+
+
+runFilterCpp :: FiltState -> Char -> (FiltState, Char) 
+runFilterCpp filtstate c = (state', charFilter (cxtFilter cxt (cfilter filtstate)) c)
+                        where (cxt, state') = charParserCpp (pchar filtstate, c) filtstate
+
+
+charParserCpp :: (String,Char) -> FiltState -> (Context, FiltState)
+
+charParserCpp (p,c) filtstate@(FiltState StateCode _ _) 
+    | p == "/"  && c == '/'  = (Code, filtstate { cstate = StateComment2, pchar = app1 p c })
+    | p == "/"  && c == '*'  = (Code, filtstate { cstate = StateComment,   pchar = app1 p c })
+    |              c == '"'  = (Code, filtstate { cstate = StateLiteral,   pchar = app1 p c })
+    |              c == '\'' = (Code, filtstate { cstate = StateLiteral2,   pchar = app1 p c }) 
+    | p == "\\" && c == '\\' = (Code, filtstate { pchar = app1 p ' ' })
+    | otherwise = (Code, filtstate { pchar = app1 p c } )
+                                       
+charParserCpp (_,c) filtstate@(FiltState StateComment2 _ _)
+    | c == '\n' = (Comment, filtstate { cstate = StateCode, pchar = app1 [] c })
+    | otherwise = (Comment, filtstate { pchar = app1 [] c })
+
+charParserCpp (p,c) filtstate@(FiltState StateComment _ _)
+    | p == "*" && c == '/'  = (Comment, filtstate { cstate = StateCode, pchar = app1 p c })
+    | otherwise = (Comment, filtstate { pchar = app1 p c })
+
+charParserCpp (p,c) filtstate@(FiltState StateLiteral _ _)
+    | p /= "\\" && c == '"'  = (Code,    filtstate { cstate = StateCode, pchar = app1 p c })
+    | p == "\\" && c == '\\' = (Literal, filtstate { pchar = app1 p ' '})
+    | otherwise = (Literal, filtstate { pchar = app1 p c }) 
+
+charParserCpp (p,c) filtstate@(FiltState StateLiteral2 _ _)
+    | p /= "\\" && c == '\'' = (Code, filtstate { cstate = StateCode, pchar = app1 p c })
+    | p == "\\" && c == '\\' = (Literal, filtstate { pchar = app1 p ' ' })
+    | otherwise = (Literal, filtstate { pchar = app1 p c})
+
 
 {-# INLINE charFilter #-}
 
 charFilter :: Bool -> Char -> Char
-charFilter  _ '\n' = '\n'
-charFilter  cond c
-    | cond = c
-    | otherwise = ' '
+charFilter  _  '\n' = '\n'
+charFilter  True  c =  c
+charFilter  _ _     = ' '
 
 
 {-# INLINE cxtFilter #-}
@@ -78,31 +159,27 @@ cxtFilter Comment = getComment
 cxtFilter Literal = getLiteral 
 
 
-charParser :: (Char,Char) -> FiltState -> (Context, FiltState)
+{-# INLINE app1 #-}
+app1 :: String -> Char -> String
+app1 _ c = [c]
 
-charParser (p,c) filtstate@(FiltState CodeState _ _) 
-    | p == '/'  && c == '/'  = (Code, filtstate { cstate = CommentCppState, pchar = c })
-    | p == '/'  && c == '*'  = (Code, filtstate { cstate = CommentCState,   pchar = c })
-    | p /= '\\' && c == '"'  = (Code, filtstate { cstate = LiteralStateS,   pchar = c })
-    | p /= '\\' && c == '\'' = (Code, filtstate { cstate = LiteralStateC,   pchar = c }) 
-    | p == '\\' && c == '\\' = (Code, filtstate { pchar = ' ' })
-    | otherwise = (Code, filtstate { pchar = c } )
-                                       
-charParser (_,c) filtstate@(FiltState CommentCppState _ _)
-    | c == '\n' = (Comment, filtstate { cstate = CodeState, pchar = c })
-    | otherwise = (Comment, filtstate { pchar = c })
+{-# INLINE app2 #-}
+app2 :: String -> Char -> String
+app2 (_:x:[]) c = [x,c]
+app2 xs c = xs ++ [c]
 
-charParser (p,c) filtstate@(FiltState CommentCState _ _)
-    | p == '*' && c == '/'  = (Comment, filtstate { cstate = CodeState, pchar = c})
-    | otherwise = (Comment, filtstate { pchar = c })
+{-# INLINE app3 #-}
+app3 :: String -> Char -> String
+app3 (_:x:y:[]) c = [x,y,c]
+app3 xs c = xs ++ [c]
 
-charParser (p,c) filtstate@(FiltState LiteralStateS _ _)
-    | p /= '\\' && c == '"'  = (Code,    filtstate { cstate = CodeState, pchar = c})
-    | p == '\\' && c == '\\' = (Literal, filtstate { pchar = ' '})
-    | otherwise = (Literal, filtstate { pchar = c }) 
+{-# INLINE app4 #-}
+app4 :: String -> Char -> String
+app4 (_:x:y:z:[]) c = [x,y,z,c]
+app4 xs c = xs ++ [c]
 
-charParser (p,c) filtstate@(FiltState LiteralStateC _ _)
-    | p /= '\\' && c == '\'' = (Code, filtstate { cstate = CodeState, pchar = c })
-    | p == '\\' && c == '\\' = (Literal, filtstate { pchar = ' '})
-    | otherwise = (Literal, filtstate { pchar = c})
+{-# INLINE app5 #-}
+app5 :: String -> Char -> String
+app5 (_:x:y:w:z:[]) c = [x,y,w,z,c]
+app5 xs c = xs ++ [c]
 
