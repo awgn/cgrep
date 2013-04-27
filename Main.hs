2a0fdf119d2e3e8ade308e9b50ec383dd6981b28
Nicola Bonelli <nicola.bonelli@gmail.com>
2013-04-27 16:36:39 +0200

    Using CmdOptions and Config module..

--- Main.hs
+++ Main.hs
@@ -18,9 +18,9 @@
 
 module Main where
 
+
 import Data.List
 import Data.Char
-import Data.Maybe
 import Data.Data()
 import Data.Function
 
@@ -30,9 +30,9 @@ import Control.Monad.STM
 import Control.Concurrent.STM.TChan
 
 import Control.Monad 
+import System.Console.CmdArgs
 import System.Directory
 import System.FilePath ((</>), takeFileName)
-import System.Console.CmdArgs
 import System.Environment
 import System.IO
 import System.Exit
@@ -42,74 +42,9 @@ import CGrep.Output
 import CGrep.CGrep
 import CGrep.Lang
 
+import CmdOptions
+
 import qualified Data.ByteString.Char8 as C
-
-cgreprc :: FilePath
-cgreprc = "cgreprc.hs" 
-
-version :: String
-version = "2.4"
-
-options = cmdArgsMode $ Options 
-          {
-                file  = ""  &= typ "FILE"  &= help "read PATTERNs from file" &= groupname "Pattern",
-                word  = False              &= help "force word matching",
-                regex = False              &= help "regex matching" &= explicit &= name "G" &=name "regex",
-                ignore_case = False        &= help "ignore case distinctions",
-
-                code = False               &= help "grep in source code"     &= explicit &= name "c" &= name "code" &= groupname "Context filters",
-                comment = False            &= help "grep in comments"        &= explicit &= name "m" &= name "comment",
-                literal = False            &= help "grep in string literals" &= explicit &= name "l" &= name "literal",
-
-                identifier = False         &= help "identifiers" &= explicit &= name "identifier" &= groupname "C/C++ language",
-                keyword = False            &= help "keywords" &= explicit &= name "keyword",
-                directive = False          &= help "preproc directives" &= explicit &= name "directive",
-                header = False             &= help "headers name" &= explicit &= name "header",
-                number = False             &= help "literal numbers" &= explicit &= name "number",
-                string = False             &= help "literal strings" &= explicit &= name "string",
-                char = False               &= help "literal chars" &= explicit &= name "char",
-                oper = False               &= help "operators" &= explicit &= name "oper",
-                
-                no_filename = False        &= help "suppress the file name prefix on output"  &= explicit &= name "h" &= name "no-filename" &= groupname "Output control",
-                no_linenumber= False       &= help "suppress the line number on output lines" &= explicit &= name "N" &= name "no-line-umber",
-                lang_map = False           &= help "show language -> ext map",
-
-                jobs   = 1                 &= help "number of jobs" &= groupname "General",
-                multiline = False          &= help "enable multi-line matching",
-                recursive = False          &= help "enable recursive search",
-                invert_match = False       &= help "select non-matching lines" &= explicit &= name "invert-match", 
-                debug = False              &= help "debug mode",
-                others = []                &= args
-
-          } &= summary ("Cgrep " ++ version ++ ". Usage: cgrep [OPTION] [PATTERN] files...") &= program "cgrep"
-
-
-data  CgrepOptions = CgrepOptions
-                    {
-                        language :: [Lang],
-                        pruneDir :: [String]
-                    } deriving (Show,Read)
-
-
--- remove # comment lines from config file
---
-
-rmCommentLines :: String -> String
-rmCommentLines =  unlines . filter (not . isPrefixOf "#" . dropWhile isSpace) . lines
-
-
--- parse CgrepOptions from ~/.cgreprc, or /etc/cgreprc 
---
-
-getCgrepOptions :: IO CgrepOptions
-getCgrepOptions = do
-    home <- getHomeDirectory
-    conf <- liftM msum $ forM [ home </> "." ++ cgreprc, "/etc" </> cgreprc ] $ \f ->
-                doesFileExist f >>= \b -> 
-                    return $ if b then Just f else Nothing
-    if isJust conf then readFile (fromJust conf) >>= \xs -> return (read (rmCommentLines xs) :: CgrepOptions) 
-                   else return $ CgrepOptions [] []
-
 
 -- from Realworld in Haskell...
 
@@ -140,9 +75,6 @@ readPatternsFromFile f =
               else liftM C.words $ C.readFile f 
 
 
-isCppIdentifier :: C.ByteString -> Bool
-isCppIdentifier = C.all (\c -> isAlphaNum c || c == '_') 
-
 
 main :: IO ()
 main = do
@@ -167,10 +99,6 @@ main = do
                                 else ps
 
 
-    -- check whether patterns require regex
-    -- opts' <- if (not $ all isCppIdentifier patterns) then putStrLn "cgrep: pattern(s) require regex search -> forced." >> return opts{ regex = True }
-    --                                                 else return opts
-    
     -- check whether is a terminal device 
     isTerm <- hIsTerminalDevice stdin
 
