a3dd7da6a30fb29077ad9b2f2ff2842511b2a8f9
Nicola Bonelli <nicola.bonelli@gmail.com>
2013-12-18 12:25:43 +0100

    Hilight algorithm improved.

--- src/CGrep/Common.hs
+++ src/CGrep/Common.hs
@@ -17,7 +17,6 @@
 --
        
 {-# LANGUAGE FlexibleContexts #-} 
-{-# LANGUAGE ExistentialQuantification #-} 
 
 module CGrep.Common (Output(..), CgrepFunction, MatchLine, Text8, 
                      getFileName, getText, getMultiLine, mkOutput, prettyOutput, spanGroup) where
@@ -27,7 +26,6 @@ import qualified Data.ByteString.Char8 as C
 import System.Console.ANSI
 
 import Control.Monad (liftM)
--- import Data.Maybe
 import Data.List
 import Data.Char
 import Data.Function
@@ -36,16 +34,19 @@ import CGrep.Types
 
 import Options
 
-
 type CgrepFunction = Options -> [Text8] -> Maybe FilePath -> IO [Output] 
 
-type Text8  = C.ByteString
 
-type Token = (Offset, String)
-type MatchLine  = (Int, [String])
+getOffsetsLines :: Text8 -> [Int]
+getOffsetsLines = C.elemIndices '\n' 
 
-data Output = Output FilePath Int Text8 [String]
-                                  
+
+getOffset2d :: [OffsetLine] -> Offset -> Offset2d
+getOffset2d idx off = let prc =  fst $ partition (< off) idx in
+        case prc of
+          [] -> (0, off)
+          _  -> (length prc, off - last prc - 1)
+
 
 getFileName :: Maybe FilePath -> String
 getFileName Nothing = "<STDIN>"
@@ -60,86 +61,22 @@ getText icase filename
  
 
 mkOutput :: Options -> FilePath -> Text8 -> [Token] -> [Output]
-mkOutput Options { invert_match = invert } f source 
-    | invert    = map (\(n, xs) -> Output f n (ls !! (n-1)) xs) . invertMatchLines (length ls) . mkMatchLines source
-    | otherwise = map (\(n, xs) -> Output f n (ls !! (n-1)) xs) . mkMatchLines source
-        where ls = C.lines source  
+mkOutput Options { invert_match = invert } f text ts
+    | invert    = map (\(n, xs) -> Output f n (ls !! (n-1)) xs) . invertMatchLines (length ls) $ mkMatchLines text ts
+    | otherwise = map (\(n, xs) -> Output f n (ls !! (n-1)) xs) $ mkMatchLines text ts
+        where ls = C.lines text  
 
 
 mkMatchLines :: Text8 -> [Token] -> [MatchLine] 
 mkMatchLines _ [] = []
-mkMatchLines src ts = map mergeGroup $ groupBy ((==) `on` fst) $ sortBy (compare `on` fst) $ map (\(o, t) -> (1 + offsetToLineNo src o, [t])) ts 
-    where mergeGroup ls = (fst $ head ls, foldl (\l m -> l ++ snd m) [] ls)  
-
-
-offsetToLineNo :: Text8 -> Int -> Int
-offsetToLineNo text = \off -> length . fst $ partition (< off) crs 
-    where crs = C.elemIndices '\n' text 
-
+mkMatchLines text ts = map mergeGroup $ groupBy ((==) `on` fst) $ sortBy (compare `on` fst) $ map (\t -> let (r,c) = getOffset2d ols (fst t) in (1 + r, [(c, snd t)])) ts 
+    where mergeGroup ls = (fst $ head ls, foldl (\l m -> l ++ snd m) [] ls)
+          ols = getOffsetsLines text
+          
 
 invertMatchLines :: Int -> [MatchLine] -> [MatchLine]
 invertMatchLines n xs =  filter (\(i,_) ->  i `notElem` idx ) $ take n [ (i, []) | i <- [1..]] 
     where idx = map fst xs
-
-
-prettyOutput :: Options -> Output -> String
-prettyOutput opt@ Options { no_filename = False, no_linenumber = False , count = False } (Output f n l ts) = showFile opt f ++ ":" ++ show n ++ ":" ++ showTokens opt ts ++ showLine opt ts l
-prettyOutput opt@ Options { no_filename = False, no_linenumber = True  , count = False } (Output f _ l ts) = showFile opt f ++ ":" ++ showTokens opt ts ++ showLine opt ts l
-prettyOutput opt@ Options { no_filename = True , no_linenumber = False , count = False } (Output _ n l ts) = show n ++ ":" ++ showTokens opt ts ++ showLine opt ts l
-prettyOutput opt@ Options { no_filename = True , no_linenumber = True  , count = False } (Output _ _ l ts) = showTokens opt ts ++ showLine opt ts l
-prettyOutput opt@ Options { count = True } (Output f n _ _) = showFile opt f ++ ":" ++ show n
-
-
-blue, bold, reset :: String
-
-blue    = setSGRCode [SetColor Foreground Vivid Blue]    
-bold    = setSGRCode [SetConsoleIntensity BoldIntensity] 
-reset   = setSGRCode []                                  
-
-
-showTokens :: Options -> [String] -> String
-showTokens Options { show_match = st } xs
-    | st        = show xs 
-    | otherwise = ""
-
-
-showFile :: Options -> String -> String
-showFile Options { color = c } f 
-    | c         = bold ++ blue ++ f ++ reset
-    | otherwise = f 
-
-
-showLine :: Options -> [String] -> Text8 -> String
-showLine Options { color = c } ts l
-    | c         = hilightLine (sortBy (flip compare `on` length) ts) (C.unpack l) 
-    | otherwise = C.unpack l 
-
-
-substrIndex :: String -> String -> (Int, [Int], Int, String)
-substrIndex reverseword = foldl step (0,[],1,"") 
-    where step (x,ys,i,matcher) c | c:matcher == reverseword = (x+1,i-length reverseword:ys,i+1,"")
-                                  | (c:matcher) `isSuffixOf` reverseword = (x,ys,i+1,c:matcher)
-                                  | otherwise = (x,ys,i+1,"")
-
-
-substrIndices :: String -> String -> [Int]
-substrIndices word content = (\(_,x,_,_)-> reverse x) $ substrIndex (reverse word) content
-
-
-substrAllIndices :: [String] -> String -> [Int]
-substrAllIndices ts xs = nub $ sort $ concatMap (`substrAllIndices'` xs) ts
-    where substrAllIndices' :: String -> String -> [Int]
-          substrAllIndices' t xs' = concatMap (\n -> take len [n..]) $ substrIndices t xs'
-            where len = length t
-        
-
-hilightLine :: [String] -> String -> String
-hilightLine ts line =  hilightLine' (idx, 0) line
-    where idx = substrAllIndices ts line
-          hilightLine' :: ([Int],Int) -> String -> String
-          hilightLine'  _ [] = []
-          hilightLine' (ns,n) (x:xs) = (if n `elem` ns then bold ++ [x] ++ reset 
-                                                       else [x]) ++ hilightLine' (ns, n+1) xs
 
 
 spanGroup :: Int -> [a] -> [[a]]
@@ -152,4 +89,51 @@ getMultiLine :: Int -> Text8 -> Text8
 getMultiLine 1 xs = xs
 getMultiLine n xs = C.unlines $ map C.unwords $ spanGroup n (C.lines xs) 
  
- 
+
+prettyOutput :: Options -> Output -> String
+prettyOutput opt@ Options { no_filename = False, no_linenumber = False , count = False } (Output f n l ts) = showFile opt f ++ ":" ++ show n ++ ":" ++ showTokens opt ts ++ showLine opt ts l
+prettyOutput opt@ Options { no_filename = False, no_linenumber = True  , count = False } (Output f _ l ts) = showFile opt f ++ ":" ++ showTokens opt ts ++ showLine opt ts l
+prettyOutput opt@ Options { no_filename = True , no_linenumber = False , count = False } (Output _ n l ts) = show n ++ ":" ++ showTokens opt ts ++ showLine opt ts l
+prettyOutput opt@ Options { no_filename = True , no_linenumber = True  , count = False } (Output _ _ l ts) = showTokens opt ts ++ showLine opt ts l
+prettyOutput opt@ Options { count = True } (Output f n _ _) = showFile opt f ++ ":" ++ show n
+
+
+blue, bold, reset :: String
+
+blue    = setSGRCode [SetColor Foreground Vivid Blue]    
+red     = setSGRCode [SetColor Foreground Vivid Red]    
+bold    = setSGRCode [SetConsoleIntensity BoldIntensity] 
+reset   = setSGRCode []                                  
+
+
+showTokens :: Options -> [Token] -> String
+showTokens Options { show_match = st } xs
+    | st        = show xs 
+    | otherwise = ""
+
+
+showFile :: Options -> String -> String
+showFile Options { color = c } f 
+    | c         = bold ++ red ++ f ++ reset
+    | otherwise = f 
+
+
+showLine :: Options -> [Token] -> Line8 -> String
+showLine Options { color = c } ts l
+    | c         = hilightLine (sortBy (flip compare `on` (length . snd )) ts) (C.unpack l) 
+    | otherwise = C.unpack l 
+
+
+hilightLine :: [Token] -> String -> String
+hilightLine ts =  hilightLine' (hilightIndicies ts, 0)
+    where hilightLine' :: ([Int],Int) -> String -> String
+          hilightLine'  _ [] = []
+          hilightLine' (ns,n) (x:xs) = (if n `elem` ns then bold ++ [x] ++ reset 
+                                                       else [x]) ++ hilightLine' (ns, n+1) xs
+
+
+
+hilightIndicies :: [Token] -> [Int]
+hilightIndicies = concatMap (\(o, s) -> take (length s) [o..])
+
+
