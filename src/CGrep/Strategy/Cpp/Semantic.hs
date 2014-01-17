7aa63475246d38c9d6da22f635ded82ccab679a5
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-01-17 11:54:43 +0100

    Added generic a WildCard data

--- src/CGrep/Strategy/Cpp/Semantic.hs
+++ src/CGrep/Strategy/Cpp/Semantic.hs
@@ -21,6 +21,7 @@ module CGrep.Strategy.Cpp.Semantic (search) where
 import qualified Data.ByteString.Char8 as C
 import qualified Data.Map.Strict as M
 
+import CGrep.WildCard
 import CGrep.Filter 
 import CGrep.Lang
 import CGrep.Common
@@ -65,7 +66,7 @@ search opt ps f = do
 
     -- get matching tokens ...
         
-    let tokens' = sortBy (compare `on` Cpp.offset) $ nub (filterMatchingTokens opt filename patterns' tokens)   
+    let tokens' = sortBy (compare `on` Cpp.offset) $ nub (filterWildCardToken opt filename patterns' tokens)   
 
     
     let matches = map (\t -> let n = fromIntegral (Cpp.offset t) in (n, Cpp.toString t)) tokens' :: [(Int, String)]
@@ -80,129 +81,30 @@ search opt ps f = do
     return $ mkOutput opt filename text matches
         
 
-data WildCard = AnyCard               |
-                KeyCard               |
-                NumberCard            |
-                OctCard               |
-                HexCard               |
-                StringCard            |
-                CharCard              |
-                IdentifierCard String |
-                WildToken Cpp.Token         
-                    deriving (Eq, Show, Ord)
-
-
-wildCardMap :: M.Map String WildCard
-wildCardMap = M.fromList 
-            [
-                ("ANY", AnyCard    ),
-                ("KEY", KeyCard    ),
-                ("OCT", OctCard    ),
-                ("HEX", HexCard    ),
-                ("NUM", NumberCard ),
-                ("CHR", CharCard   ),
-                ("STR", StringCard )
-            ]
-
-
-mkWildCard :: Cpp.Token -> WildCard
-mkWildCard t@Cpp.TokenIdentifier { Cpp.toString = str } 
-    | Just wc <-  M.lookup str wildCardMap =  wc 
-    | ('$':_) <- str                       = IdentifierCard str
-    | ('_':_) <- str                       = IdentifierCard str
-    | otherwise                            = WildToken t
-mkWildCard t = WildToken t
-
-
-getWildCardSubsequence :: [WildCard] -> [[WildCard]]
-getWildCardSubsequence wc = map (`filterCardIndicies` wc') idx
-    where wc' = zip [0..] wc
-          idx = subsequences $ findIndices (\w -> case w of 
-                                                   IdentifierCard ('$':_) -> True  
-                                                   _ -> False) wc
-
-
-filterCardIndicies :: [Int] -> [(Int,WildCard)] -> [WildCard]
-filterCardIndicies ns ps = map snd $ filter (\(n, _) -> n `notElem` ns) ps
-
-
-filterMatchingTokens :: Options -> FilePath -> [[WildCard]] -> [Cpp.Token] -> [Cpp.Token]
-filterMatchingTokens _ _ [] _ = []
-filterMatchingTokens opt f (g:gs) ts = 
-    concatMap (take grpLen . (`drop` ts)) (findIndices (groupCompare opt g) grp) ++ 
-        filterMatchingTokens opt f gs ts 
-    where grp    = spanGroup grpLen ts
-          grpLen = length g
-
-
-groupCompare :: Options -> [WildCard] -> [Cpp.Token] -> Bool
-groupCompare opt l r = 
-    groupCompareAll ts && groupCompareOptional ts
-        where ts = tokensGroupCompare opt l r               
-
-
-{-# INLINE groupCompareAll #-}
-
-groupCompareAll :: [(Bool, (WildCard, [String]))] -> Bool
-groupCompareAll = all fst 
-
-
-{-# INLINE groupCompareOptional #-}
-
--- Note: pattern $ and _ match any token, whereas $1 $2 (_1 _2 etc.) match tokens
---       that must compare equal in the relative occurrences  
---       
-
-groupCompareOptional :: [(Bool, (WildCard, [String]))] -> Bool
-groupCompareOptional ts =  M.foldr (\xs r -> r && all (== head xs) xs) True m  
-        where m =  M.mapWithKey (\k xs -> case k of 
-                                                AnyCard             -> [] 
-                                                NumberCard          -> [] 
-                                                KeyCard             -> []
-                                                StringCard          -> []
-                                                CharCard            -> []
-                                                HexCard             -> []
-                                                OctCard             -> []
-                                                IdentifierCard "_"  -> []
-                                                IdentifierCard "$"  -> []
-                                                _                   -> xs
-                                  ) $ M.fromListWith (++) (map snd ts)
-        
-
-tokensGroupCompare :: Options -> [WildCard] -> [Cpp.Token] -> [(Bool, (WildCard, [String]))]
-tokensGroupCompare opt ls rs 
-    | length rs >= length ls = zipWith (tokensZip opt) ls rs
-    | otherwise = [ (False, (AnyCard, [])) ]
-
-
-tokensZip :: Options -> WildCard -> Cpp.Token -> (Bool, (WildCard, [String]))
-tokensZip opt l r 
-    |  wiildCardCompare opt l r = (True, (l, [Cpp.toString r]))
-    |  otherwise                = (False,(AnyCard,[] ))
-
-
-wiildCardCompare :: Options -> WildCard -> Cpp.Token -> Bool
-
-wiildCardCompare _  (IdentifierCard _) (Cpp.TokenIdentifier {}) = True
-wiildCardCompare _  AnyCard _                                   = True
-wiildCardCompare _  KeyCard    (Cpp.TokenKeyword {}) = True
-wiildCardCompare _  StringCard (Cpp.TokenString  {}) = True
-wiildCardCompare _  CharCard   (Cpp.TokenChar    {}) = True
-wiildCardCompare _  NumberCard (Cpp.TokenNumber  {}) = True
-wiildCardCompare _  OctCard    (Cpp.TokenNumber  { Cpp.toString = r }) = case r of ('0':d: _) -> isDigit d; _ -> False
-wiildCardCompare _  HexCard    (Cpp.TokenNumber  { Cpp.toString = r }) = case r of ('0':'x':_) -> True; _ -> False
-
-wiildCardCompare opt (WildToken Cpp.TokenIdentifier {Cpp.toString = l}) (Cpp.TokenIdentifier {Cpp.toString = r}) 
-    | edit_dist  opt =  l ~== r
-    | word_match opt =  l == r
-    | otherwise      =  l `isInfixOf` r
-
-wiildCardCompare opt (WildToken Cpp.TokenString     {Cpp.toString = l}) (Cpp.TokenString     {Cpp.toString = r}) 
-    | edit_dist  opt =  l ~== r
-    | word_match opt =  l == r
-    | otherwise      =  trim l `isInfixOf` r
-        where trim = init . tail
-
-wiildCardCompare _  (WildToken l) r = Cpp.tokenCompare l r 
-wiildCardCompare _ _ _ = False
+instance GenericToken Cpp.Token where
+    isIdentifier    = Cpp.isIdentifier
+    getString       = Cpp.toString
+    
+    wildCardMatch _  (IdentifCard _) (Cpp.TokenIdentifier {}) = True
+    wildCardMatch _  AnyCard _                                   = True
+    wildCardMatch _  KeyWordCard (Cpp.TokenKeyword {}) = True
+    wildCardMatch _  StringCard  (Cpp.TokenString  {}) = True
+    wildCardMatch _  CharCard    (Cpp.TokenChar    {}) = True
+    wildCardMatch _  NumberCard  (Cpp.TokenNumber  {}) = True
+    wildCardMatch _  OctCard     (Cpp.TokenNumber  { Cpp.toString = r }) = case r of ('0':d: _) -> isDigit d; _ -> False
+    wildCardMatch _  HexCard     (Cpp.TokenNumber  { Cpp.toString = r }) = case r of ('0':'x':_) -> True; _     -> False
+    
+    wildCardMatch opt (TokenCard Cpp.TokenIdentifier {Cpp.toString = l}) (Cpp.TokenIdentifier {Cpp.toString = r}) 
+        | edit_dist  opt =  l ~== r
+        | word_match opt =  l == r
+        | otherwise      =  l `isInfixOf` r
+    
+    wildCardMatch opt (TokenCard Cpp.TokenString     {Cpp.toString = l}) (Cpp.TokenString     {Cpp.toString = r}) 
+        | edit_dist  opt =  l ~== r
+        | word_match opt =  l == r
+        | otherwise      =  trim l `isInfixOf` r
+            where trim = init . tail
+    
+    wildCardMatch _  (TokenCard l) r = Cpp.tokenCompare l r 
+    wildCardMatch _ _ _ = False
 
---
+++ src/CGrep/WildCard.hs
@@ -0,0 +1,147 @@
+--
+-- Copyright (c) 2013 Bonelli Nicola <bonelli@antifork.org>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+
+module CGrep.WildCard (WildCard(..), GenericToken(..), 
+                       mkWildCard, 
+                       getWildCardSubsequence,
+                       filterWildCardToken) where
+
+import qualified Data.Map as M
+import CGrep.Filter 
+import CGrep.Lang
+import CGrep.Common
+import CGrep.Output
+import CGrep.Distance
+
+import Data.Char
+import Data.List
+import Data.Function
+
+import Options 
+import Debug
+
+
+class (Show t, Ord t) => GenericToken t where
+    isIdentifier    :: t -> Bool
+    getString       :: t -> String
+    wildCardMatch :: Options ->  WildCard t -> t -> Bool
+
+
+data WildCard a =  TokenCard a          |
+                   AnyCard              |
+                   KeyWordCard          |
+                   NumberCard           |
+                   OctCard              |
+                   HexCard              |
+                   StringCard           |
+                   CharCard             |
+                   IdentifCard String 
+                       deriving (Show, Eq, Ord)      
+
+
+wildCardMap :: M.Map String (WildCard a) 
+wildCardMap = M.fromList 
+            [
+                ("ANY", AnyCard    ),
+                ("KEY", KeyWordCard),
+                ("OCT", OctCard    ),
+                ("HEX", HexCard    ),
+                ("NUM", NumberCard ),
+                ("CHR", CharCard   ),
+                ("STR", StringCard )
+            ]
+
+
+mkWildCard :: (GenericToken a) => a -> WildCard a
+mkWildCard  t
+    | isIdentifier t = case () of 
+                        _  |  Just wc <-  M.lookup str wildCardMap -> wc
+                           | ('$':_)  <- getString t               -> IdentifCard str
+                           | ('_':_)  <- getString t               -> IdentifCard str
+                           | otherwise                             -> TokenCard t
+    | otherwise      = TokenCard t
+        where str = getString t
+
+
+getWildCardSubsequence :: [WildCard a] -> [[WildCard a]]
+getWildCardSubsequence wc = map (`filterCardIndicies` wc') idx
+    where wc' = zip [0..] wc
+          idx = subsequences $ 
+                findIndices (\w -> case w of 
+                                    IdentifCard ('$':_) -> True  
+                                    _ -> False) wc
+
+
+filterCardIndicies :: [Int] -> [(Int,WildCard a)] -> [WildCard a]
+filterCardIndicies ns ps = map snd $ filter (\(n, _) -> n `notElem` ns) ps
+
+
+filterWildCardToken :: (GenericToken a) => Options -> FilePath -> [[WildCard a]] -> [a] -> [a]
+filterWildCardToken _ _ [] _ = []
+filterWildCardToken opt f (g:gs) ts = 
+    concatMap (take grpLen . (`drop` ts)) (findIndices (groupCompare opt g) grp) ++ 
+        filterWildCardToken opt f gs ts 
+    where grp    = spanGroup grpLen ts
+          grpLen = length g
+
+
+groupCompare :: (GenericToken a) => Options -> [WildCard a] -> [a] -> Bool
+groupCompare opt l r = 
+    groupCompareAll ts && groupCompareOptional ts
+        where ts = tokensGroupCompare opt l r               
+
+
+{-# INLINE groupCompareAll #-}
+
+groupCompareAll :: [(Bool, (WildCard a, [String]))] -> Bool
+groupCompareAll = all fst 
+
+
+{-# INLINE groupCompareOptional #-}
+
+-- Note: pattern $ and _ match any token, whereas $1 $2 (_1 _2 etc.) match tokens
+--       that must compare equal in the relative occurrences  
+--       
+
+groupCompareOptional :: (GenericToken a) => [(Bool, (WildCard a, [String]))] -> Bool
+groupCompareOptional ts =  M.foldr (\xs r -> r && all (== head xs) xs) True m  
+        where m =  M.mapWithKey (\k xs -> case k of 
+                                                AnyCard          -> [] 
+                                                NumberCard       -> [] 
+                                                KeyWordCard      -> []
+                                                StringCard       -> []
+                                                CharCard         -> []
+                                                HexCard          -> []
+                                                OctCard          -> []
+                                                IdentifCard "_"  -> []
+                                                IdentifCard "$"  -> []
+                                                _                -> xs
+                                  ) $ M.fromListWith (++) (map snd ts)
+        
+
+tokensGroupCompare :: (GenericToken a) => Options -> [WildCard a] -> [a] -> [(Bool, (WildCard a, [String]))]
+tokensGroupCompare opt ls rs 
+    | length rs >= length ls = zipWith (tokensZip opt) ls rs
+    | otherwise = [ (False, (AnyCard, [])) ]
+
+
+tokensZip :: (GenericToken a) => Options -> WildCard a -> a -> (Bool, (WildCard a, [String]))
+tokensZip opt l r 
+    |  wildCardMatch opt l r = (True, (l, [getString r]))
+    |  otherwise             = (False,(AnyCard,[] ))
+
