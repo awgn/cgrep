83af4c6683f1057d19344a82aace862ef83597b9
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-01-29 14:30:42 +0100

    In CGrep/Token.hs, switched to DList

--- src/CGrep/Token.hs
+++ src/CGrep/Token.hs
@@ -22,12 +22,14 @@
 module CGrep.Token (tokens, tokenizer) where
 
 import qualified Data.ByteString.Char8 as C
+import qualified Data.DList as DL
 
 import Data.Char
 import Data.Array.Unboxed
 
 import CGrep.Types
 
+type DString = DL.DList Char
 
 data TokenState = TokenSpace |
                   TokenAlpha |
@@ -36,8 +38,7 @@ data TokenState = TokenSpace |
                     deriving (Eq, Enum, Show)
 
 
-data TokenAccum = TokenAccum !TokenState !Offset String [Token]
-    deriving (Show,Eq)
+data TokenAccum = TokenAccum !TokenState !Offset DString (DL.DList Token)
 
 
 isCharNumberLT :: UArray Char Bool
@@ -73,39 +74,41 @@ tokens = map snd . tokenizer
 
 {-# INLINE mkToken #-}
 
-mkToken :: Offset -> String -> Token
-mkToken off acc =  (off - length acc, reverse acc)
 
+mkToken :: Offset -> DString -> Token
+mkToken off ds =  (off - length str, str)
+    where str = DL.toList ds
 
 tokenizer :: Text8 -> [Token]
-tokenizer xs = (\(TokenAccum _  off acc out) -> if null acc then out else reverse out ++ [mkToken off acc]) $ C.foldl' tokens' (TokenAccum TokenSpace 0 "" []) xs
+tokenizer xs = (\(TokenAccum _  off acc out) -> DL.toList (out `DL.snoc` mkToken off acc)) $ C.foldl' tokens' (TokenAccum TokenSpace 0 DL.empty DL.empty) xs
     where tokens' :: TokenAccum -> Char -> TokenAccum
           tokens' (TokenAccum TokenSpace off acc out) x =
               case () of
-                _  | isSpace x                ->  TokenAccum TokenSpace (off+1) acc out
-                   | isAlpha x || x == '_'    ->  TokenAccum TokenAlpha (off+1) (x : acc) out
-                   | isDigit x                ->  TokenAccum TokenDigit (off+1) (x : acc) out
-                   | otherwise                ->  TokenAccum TokenOther (off+1) (x : acc) out
+                _  | isSpaceLT ! x      ->  TokenAccum TokenSpace (off+1)  acc              out
+                   | isAlphaLT ! x      ->  TokenAccum TokenAlpha (off+1) (acc `DL.snoc` x) out
+                   | isDigitLT ! x      ->  TokenAccum TokenDigit (off+1) (acc `DL.snoc` x) out
+                   | otherwise          ->  TokenAccum TokenOther (off+1) (acc `DL.snoc` x) out
 
           tokens' (TokenAccum TokenAlpha off acc out) x =
               case () of
-                _  | isSpace x                ->  TokenAccum TokenSpace (off+1) ""  (mkToken off acc : out)
-                   | isAlphaNum x || x == '_' ->  TokenAccum TokenAlpha (off+1) (x : acc) out
-                   | otherwise                ->  TokenAccum TokenOther (off+1) [x] (mkToken off acc : out)
+                _  | isAlphaNumLT ! x   ->  TokenAccum TokenAlpha (off+1) (acc `DL.snoc` x)  out
+                   | isSpaceLT ! x      ->  TokenAccum TokenSpace (off+1) DL.empty          (out `DL.snoc` mkToken off acc)
+                   | otherwise          ->  TokenAccum TokenOther (off+1) (DL.singleton  x) (out `DL.snoc` mkToken off acc)
 
           tokens' (TokenAccum TokenDigit off acc out) x =
               case () of
-                _  | isSpace x                ->  TokenAccum TokenSpace (off+1) ""  (mkToken off acc : out)
-                   | isCharNumber x           ->  TokenAccum TokenDigit (off+1) (x : acc) out
-                   | isAlpha x || x == '_'    ->  TokenAccum TokenAlpha (off+1) [x] (mkToken off acc : out)
-                   | otherwise                ->  TokenAccum TokenOther (off+1) [x] (mkToken off acc : out)
+                _  | isCharNumberLT ! x ->  TokenAccum TokenDigit (off+1) (acc `DL.snoc` x)  out
+                   | isSpaceLT ! x      ->  TokenAccum TokenSpace (off+1) DL.empty          (out `DL.snoc` mkToken off acc)
+                   | isAlphaLT ! x      ->  TokenAccum TokenAlpha (off+1) (DL.singleton  x) (out `DL.snoc` mkToken off acc)
+                   | otherwise          ->  TokenAccum TokenOther (off+1) (DL.singleton  x) (out `DL.snoc` mkToken off acc)
 
           tokens' (TokenAccum TokenOther off acc out) x =
               case () of
-                _  | isSpace x                ->  TokenAccum TokenSpace (off+1) ""  (mkToken off acc : out)
-                   | isAlpha x || x == '_'    ->  TokenAccum TokenAlpha (off+1) [x] (mkToken off acc : out)
-                   | isDigit x                ->  if acc == "." then TokenAccum TokenDigit (off+1) (x : ".") out
-                                                                else TokenAccum TokenDigit (off+1) [x] (mkToken off acc : out)
-                   | otherwise                ->  TokenAccum TokenOther (off+1) (x : acc) out
+                _  | isSpaceLT ! x      ->  TokenAccum TokenSpace (off+1) DL.empty          (out `DL.snoc` mkToken off acc)
+                   | isAlphaLT ! x      ->  TokenAccum TokenAlpha (off+1) (DL.singleton x)  (out `DL.snoc` mkToken off acc)
+                   | isDigitLT ! x      ->  if DL.toList acc == "."
+                                            then TokenAccum TokenDigit (off+1) (acc `DL.snoc` x)  out
+                                            else TokenAccum TokenDigit (off+1) (DL.singleton  x) (out `DL.snoc` mkToken off acc)
+                   | otherwise          ->  TokenAccum TokenOther      (off+1) (acc `DL.snoc` x)  out
 
 
