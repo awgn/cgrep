e5daef701c833e7ea2f7039deb53189ebd62b7b0
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-01-30 15:10:34 +0100

    Re-added simple tokenizer, which was previously implemented in-term-of the generic semantic tokenizer

--- src/CGrep/Token.hs
+++ src/CGrep/Token.hs
@@ -21,8 +21,11 @@
 
 module CGrep.Token (Token, MatchLine, tokens, tokenizer) where
 
+import qualified Data.ByteString.Char8 as C
+import qualified Data.DList as DL
 
-import qualified CGrep.Semantic.Generic.Token as G
+import Data.Char
+import Data.Array.Unboxed
 
 import CGrep.Types
 
@@ -30,11 +33,103 @@ import CGrep.Types
 type Token      = (Offset, String)
 type MatchLine  = (OffsetLine, [Token])
 
+type DString    = DL.DList Char
+
+
+data TokenState = StateSpace   |
+                  StateAlpha   |
+                  StateDigit   |
+                  StateBracket |
+                  StateOther
+                    deriving (Eq, Enum, Show)
+
+data TokenAccum = TokenAccum !TokenState !Offset DString (DL.DList Token)
+
+
+isCharNumberLT :: UArray Char Bool
+isCharNumberLT =
+    listArray ('\0', '\255')
+        (map (\c -> isHexDigit c || c `elem` ['.', 'x','X']) ['\0'..'\255'])
+
+
+isSpaceLT :: UArray Char Bool
+isSpaceLT =
+    listArray ('\0', '\255')
+        (map (\c -> isSpace c) ['\0'..'\255'])
+
+isAlphaLT :: UArray Char Bool
+isAlphaLT =
+    listArray ('\0', '\255')
+        (map (\c -> isAlpha c || c == '_') ['\0'..'\255'])
+
+isAlphaNumLT :: UArray Char Bool
+isAlphaNumLT =
+    listArray ('\0', '\255')
+        (map (\c -> isAlphaNum c || c == '_' || c == '\'') ['\0'..'\255'])
+
+isDigitLT :: UArray Char Bool
+isDigitLT =
+    listArray ('\0', '\255')
+        (map (\c -> isDigit c) ['\0'..'\255'])
+
+isBracketLT :: UArray Char Bool
+isBracketLT =
+    listArray ('\0', '\255')
+        (map (\c -> c `elem` "{[()]}" ) ['\0'..'\255'])
+
+
+{-# INLINE mkToken #-}
+
+mkToken :: Offset -> DString -> Token
+mkToken off ds =  ((off - length str), str)
+    where str = DL.toList ds
+
 
 tokens :: Text8 -> [String]
-tokens = map G.toString . G.tokenizer
+tokens = (map snd) . tokenizer
 
 
 tokenizer :: Text8 -> [Token]
-tokenizer = map (\t -> (G.offset t, G.toString t)) . G.tokenizer
+tokenizer xs = (\(TokenAccum _ off acc out) -> DL.toList (if null (DL.toList acc) then out
+                                                                                  else out `DL.snoc` mkToken off acc)) $ C.foldl' tokens' (TokenAccum StateSpace 0 DL.empty DL.empty) xs
+    where tokens' :: TokenAccum -> Char -> TokenAccum
+          tokens' (TokenAccum StateSpace off _ out) x =
+              case () of
+                _  | isSpaceLT ! x      ->  TokenAccum StateSpace   (off+1)  DL.empty         out
+                   | isAlphaLT ! x      ->  TokenAccum StateAlpha   (off+1) (DL.singleton  x) out
+                   | isDigitLT ! x      ->  TokenAccum StateDigit   (off+1) (DL.singleton  x) out
+                   | isBracketLT ! x    ->  TokenAccum StateBracket (off+1) (DL.singleton  x) out
+                   | otherwise          ->  TokenAccum StateOther   (off+1) (DL.singleton  x) out
 
+          tokens' (TokenAccum StateAlpha off acc out) x =
+              case () of
+                _  | isAlphaNumLT ! x   ->  TokenAccum StateAlpha   (off+1) (acc `DL.snoc` x)  out
+                   | isSpaceLT ! x      ->  TokenAccum StateSpace   (off+1)  DL.empty         (out `DL.snoc` mkToken off acc)
+                   | isBracketLT ! x    ->  TokenAccum StateBracket (off+1) (DL.singleton  x) (out `DL.snoc` mkToken off acc)
+                   | otherwise          ->  TokenAccum StateOther   (off+1) (DL.singleton  x) (out `DL.snoc` mkToken off acc)
+
+          tokens' (TokenAccum StateDigit off acc out) x =
+              case () of
+                _  | isCharNumberLT ! x ->  TokenAccum StateDigit   (off+1) (acc `DL.snoc` x)  out
+                   | isSpaceLT ! x      ->  TokenAccum StateSpace   (off+1)  DL.empty         (out `DL.snoc` mkToken off acc)
+                   | isAlphaLT ! x      ->  TokenAccum StateAlpha   (off+1) (DL.singleton  x) (out `DL.snoc` mkToken off acc)
+                   | isBracketLT ! x    ->  TokenAccum StateBracket (off+1) (DL.singleton  x) (out `DL.snoc` mkToken off acc)
+                   | otherwise          ->  TokenAccum StateOther   (off+1) (DL.singleton  x) (out `DL.snoc` mkToken off acc)
+
+          tokens' (TokenAccum StateBracket off acc out) x =
+              case () of
+                _  | isSpaceLT ! x      ->  TokenAccum StateSpace   (off+1)  DL.empty         (out `DL.snoc` mkToken off acc)
+                   | isAlphaLT ! x      ->  TokenAccum StateAlpha   (off+1) (DL.singleton  x) (out `DL.snoc` mkToken off acc)
+                   | isDigitLT ! x      ->  TokenAccum StateDigit   (off+1) (DL.singleton  x) (out `DL.snoc` mkToken off acc)
+                   | isBracketLT ! x    ->  TokenAccum StateBracket (off+1) (DL.singleton  x) (out `DL.snoc` mkToken off acc)
+                   | otherwise          ->  TokenAccum StateOther   (off+1) (DL.singleton  x) (out `DL.snoc` mkToken off acc)
+
+          tokens' (TokenAccum StateOther off acc out) x =
+              case () of
+                _  | isSpaceLT ! x      ->  TokenAccum StateSpace   (off+1)  DL.empty         (out `DL.snoc` mkToken off acc)
+                   | isAlphaLT ! x      ->  TokenAccum StateAlpha   (off+1) (DL.singleton x)  (out `DL.snoc` mkToken off acc)
+                   | isDigitLT ! x      ->  if DL.toList acc == "."
+                                            then TokenAccum StateDigit (off+1) (acc `DL.snoc` x)  out
+                                            else TokenAccum StateDigit (off+1) (DL.singleton  x) (out `DL.snoc` mkToken off acc)
+                   | isBracketLT ! x    ->  TokenAccum StateBracket    (off+1) (DL.singleton  x) (out `DL.snoc` mkToken off acc)
+                   | otherwise          ->  TokenAccum StateOther      (off+1) (acc `DL.snoc` x)  out
