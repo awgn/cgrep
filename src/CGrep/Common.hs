8737c018840ff10787af018bf3af19a7876aef09
Nicola Bonelli <nicola.bonelli@gmail.com>
2016-01-26 10:03:43 +0100

    Speed of quickSearch function improved (filter must match all pre-processed quick-token list).

--- src/CGrep/Common.hs
+++ src/CGrep/Common.hs
@@ -58,9 +58,9 @@ getTargetContents xs = C.readFile xs
 
 quickSearch :: Options -> [Text8] -> Text8 -> Maybe Bool
 quickSearch opt ps text
-    | no_turbo opt        = Nothing
-    | otherwise           = Just $ any has_pattern ps || null ps
-    where has_pattern pat = notNull $ pat `SC.nonOverlappingIndices` text
+    | no_quick opt        = Nothing
+    | otherwise           = Just $ all findToken ps || null ps
+    where findToken tok = notNull $ tok `SC.nonOverlappingIndices` text
 
 
 runQuickSearch :: FilePath
